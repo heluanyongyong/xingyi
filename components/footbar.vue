959c033fb0f3bf6f5c8f5a6a7b504d3f70502c71
suyan <936737951@qq.com>
2018-12-07 17:26:54 +0800

    12.07 su2

--- components/footbar.vue
+++ components/footbar.vue
@@ -21,7 +21,17 @@
 	    			<li><a href="javascript:void(0);">联系我们</a></li>
 	    		</ul>
 	    		<div class="clear"></div>
-	    		<p class="comment">网络文化经营许可证：粤网文〔2017〕3969-775号</p>
+	    		<!-- <p class="comment">网络文化经营许可证：粤网文〔2017〕3969-775号</p> -->
+	    		<ul class="nav_list">
+	    			<li><a target="_blank" href="/credential/">营业执照</a></li>
+	    			<li>|</li>
+	    			<li><a target="_blank" href="/credential/network">网络文化经营许可证</a></li>
+	    			<li>|</li>
+	    			<li><a target="_blank" href="/credential/computer">计算机软件著作权登记证书</a></li>
+	    			<li>|</li>
+	    			<li><a target="_blank" href="/credential/up">增值电信业务经营许可证</a></li>
+	    		</ul>
+	    		<div class="clear"></div>
 	    		<p class="comment">Copyright © 2015-2018 WanPlus. All rights reserved | 深圳星移网络科技有限公司</p>
 	    		<p class="comment">抵制不良游戏 拒绝盗版游戏 注意自我保护 谨防受骗上当 适度游戏益脑 沉迷游戏伤身 合理安排时间 享受健康生活</p>
     		</div>
---
+++ pages/credential/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="ptb3" style="background-color: #525659">
+		<div class="container tc">
+			<img src="/credential/one.png" style="width: 60%">
+		</div>
+	</div>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default{
+	layout:'null',
+	head:{
+		title:'营业执照',
+		link: [
+	      { rel: 'shortcut icon', type: 'image/x-icon', href: 'favicon.ico' }
+	    ]
+	}
+}
+</script>
