55b49e61a508b31d71973187e94e29b3e82a5762
suyan <936737951@qq.com>
2018-12-14 16:33:38 +0800

    12.14 sy

--- pages/games_center/one_game.vue
+++ pages/games_center/one_game.vue
@@ -1,12 +1,12 @@
 <template>
 	<div>
-		<!-- <div class="w100">
-			<nuxt-link to="/games_center/infos"><img src="/games_center/banner1.png" alt=""></nuxt-link>
+		<div class="w100">
+			<nuxt-link to="/games_center/infos"><img src="/games_center/banner1.png" alt="" class="w100"></nuxt-link>
 		</div>
 		<div class="tc ptb3">
 			<nuxt-link to="/games_center/infos"><img src="/games_center/game.png" alt=""></nuxt-link>
-		</div> -->
-		<el-row>
+		</div>
+		<!-- <el-row>
 			<el-col :xs="24" :lg="24" :md="24" class="w100">
 				<nuxt-link to="/games_center/infos"><img src="/games_center/banner1.png" alt=""></nuxt-link>
 			</el-col>
@@ -15,7 +15,7 @@
 			<el-col class="tc ptb3" :xs="24" :lg="24" :md="24">
 				<nuxt-link to="/games_center/infos"><img src="/games_center/game.png" alt=""></nuxt-link>
 			</el-col>
-		</el-row>
+		</el-row> -->
 	</div>
 </template>
 <style scoped>
